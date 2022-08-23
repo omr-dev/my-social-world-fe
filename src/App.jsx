b1c94c0f2f7d2b291ddec6447644c57b7f3f9ddb
omer <jagenau49@gmail.com>
2022-08-23 08:42:48 +0200

    home page draft created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.scss";
+import { Home } from "./pages/home/Home";
 
 function App() {
   return (
     <div className="App">
       <h1>My Social World</h1>
+      <Home />
     </div>
   );
 }
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import "./home.scss";
+export const Home = () => {
+  return (
+    <div className="container">
+      HOME PAGE
+      <div className="home-page">
+        <div className="home-page-left"></div>
+        <div className="home-page-right"></div>
+      </div>
+    </div>
+  );
+};
